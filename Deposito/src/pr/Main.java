04933fae63e17192e04005356d7564a99b870d19
fjgarciasanz <jevoir@gmail.com>
2024-02-08 13:23:06 +0100

    Tarea modificada

--- Deposito/src/pr/Main.java
+++ Deposito/src/pr/Main.java
@@ -2,24 +2,39 @@ package pr;
 
 public class Main {
 
+    private static  Cuenta1 miCuenta = new Cuenta1("Antonio Lopez","1000-2365-85-1230456789",2500,0);
+    private static double saldoActual ;
     public static void main(String[] args) {
-        CCuenta miCuenta;
-        double saldoActual;
-
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
-
-        try {
-            miCuenta.retirar(2300);
+       
+  
+        operativa_cuenta(0,3);
+        operativa_cuenta(2300,1);
+        operativa_cuenta(695,2);
+      }
+    
+    
+    private static void operativa_cuenta(float cantidad, int i){
+        
+     switch(i){
+         case 1:
+      try {
+            miCuenta.retirar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
+         break;
+         case 2:
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            miCuenta.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
+        break;
+         case 3:
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        break;
     }
+    } 
 }
